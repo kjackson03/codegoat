ac855b4c737372372a464ce62f9dfd95f1c2f759
Kevin Jackson <kjackson03@gmail.com>
2023-04-06 11:46:28 -0500

    Change secret in main.tf

--- secrets/main.tf
+++ secrets/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   # checkov:skip=CKV_SECRET_2:nah
   access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "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"
+ # secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMAAAKEY"
+  secret_key = $SECRET_KEY
 }
